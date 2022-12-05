9edf93bbe5c4eb322a84a20b9df462bcf515f6d6
Marjanamiiri <marjanamiri799@gmail.com>
2022-12-04 16:51:10 -0800

    phase 2

---
+++ scripts/finalproject.js
@@ -0,0 +1,137 @@
+const button = document.getElementById('new-dice')
+const newGame = document.getElementById('new-game')
+
+const dice1 = document.getElementById('dice1-1')
+const dice2 = document.getElementById('dice1-2')
+
+const dice3 = document.getElementById('dice2-1')
+const dice4 = document.getElementById('dice2-2')
+
+const currentScore1 = document.getElementById('current-score1')
+const totalScore1 = document.getElementById('total-score1')
+
+const currentScore2 = document.getElementById('current-score2')
+const totalScore2 = document.getElementById('total-score2')
+
+const howToPlay = document.getElementById('how-play')
+
+const popUp1 = document.getElementById("pop-rules");
+
+const closePopUp = document.getElementById('demo1')
+
+const winPopup = document.getElementById('win-popup')
+const lostPopup = document.getElementById('lost-popup')
+const drawPopup = document.getElementById('draw-popup')
+
+popUp1.style.display = 'none'
+
+howToPlay.addEventListener("click", function (){
+    lostPopup.style.display  = 'none';
+    winPopup.style.display   = 'none';
+    drawPopup.style.display  = 'none';
+
+    $("#pop-rules").css({opacity:'0'});
+    $("#pop-rules").stop().animate({opacity:'1'},100);
+    popUp1.style.display = 'block'
+
+})
+closePopUp.addEventListener("click", function (){
+    $("#pop-rules").css({opacity:'1'});
+    $("#pop-rules").stop().animate({opacity:'0'},100);
+    popUp1.style.display = 'none'
+})
+
+
+newGame.addEventListener("click", function(){
+    location.reload();
+})
+
+button.addEventListener("click", rollDices);
+
+let p1TotalScore = 0;
+let p2TotalScore = 0;
+let counter      = 0;
+
+function rollDices(){
+
+  let p1CurrentScore = "";
+  let p2CurrentScore = "";
+
+    one = Math.round(Math.random()*5) + 1;
+    two = Math.round(Math.random()*5) + 1;
+    three = Math.round(Math.random()*5) + 1;
+    four = Math.round(Math.random()*5) + 1;
+
+    dice1.src = "images/dice-" + one   + ".png";
+    dice2.src = "images/dice-" + two   + ".png";
+    dice3.src = "images/dice-" + three + ".png";
+    dice4.src = "images/dice-" + four  + ".png";
+
+    if(one === 1){
+        p1CurrentScore = 0;
+    }else if(two === 1){
+        p1CurrentScore = 0;
+    }else{
+        p1CurrentScore = one + two;
+    }
+    if(one === two){
+        p1CurrentScore = (one + two) * 2;
+    }
+    if(three === 1){
+        p2CurrentScore = 0;
+        
+    }else if(four === 1){
+        p2CurrentScore = 0;
+    }else{
+        p2CurrentScore = three + four;
+    }
+    if(three === four){
+        p2CurrentScore = (three + four) * 2;
+    }
+
+    p1TotalScore = p1TotalScore + p1CurrentScore;
+    p2TotalScore = p2TotalScore + p2CurrentScore;
+  
+    currentScore1.innerHTML = `${p1CurrentScore}`;
+    totalScore1.innerHTML   = `${p1TotalScore}`;
+    currentScore2.innerHTML = `${p2CurrentScore}`;
+    totalScore2.innerHTML   = `${p2TotalScore}`;
+    
+    counter++;
+    
+    if(counter >= 3){
+        button.disabled = true;
+      
+      if(p1TotalScore > p2TotalScore){
+
+        $("#win-popup").css({opacity:'0'});
+        $("#win-popup").stop().animate({opacity:'1'},1000);
+        newGame.classList.add('restBtn')
+        button.classList.add('restRoll')
+        winPopup.style.display = 'block';
+        // winPopup.style.backgroundColor = 'yellowgreen';
+        
+        
+
+      }else if(p2TotalScore > p1TotalScore){
+
+        $("#lost-popup").css({opacity:'0'});
+        $("#lost-popup").stop().animate({opacity:'1'},1000);
+        newGame.classList.add('restBtn')
+        button.classList.add('restRoll')
+        lostPopup.style.display = 'block';  
+
+      }else{
+
+        $("#draw-popup").css({opacity:'0'});
+        $("#draw-popup").stop().animate({opacity:'1'},1000);
+        newGame.classList.add('restBtn')
+        button.classList.add('restRoll')
+        drawPopup.style.display = 'block';
+        // drawPopup.style.backgroundColor = 'gray';
+      }
+    }
+}
+
+
+
