b9820af663d03c8bc31cc6a73d5f155ef41aaba7
Marjanamiiri <marjanamiri799@gmail.com>
2022-12-06 13:06:23 -0800

    add all the file inside code folder

--- code/scripts/finalproject.js
+++ code/scripts/finalproject.js
@@ -1,4 +1,4 @@
-const button = document.getElementById('new-dice')
+const button  = document.getElementById('new-dice')
 const newGame = document.getElementById('new-game')
 
 const dice1 = document.getElementById('dice1-1')
@@ -8,20 +8,16 @@ const dice3 = document.getElementById('dice2-1')
 const dice4 = document.getElementById('dice2-2')
 
 const currentScore1 = document.getElementById('current-score1')
-const totalScore1 = document.getElementById('total-score1')
-
+const totalScore1   = document.getElementById('total-score1')
 const currentScore2 = document.getElementById('current-score2')
-const totalScore2 = document.getElementById('total-score2')
+const totalScore2   = document.getElementById('total-score2')
 
-const howToPlay = document.getElementById('how-play')
-
-const popUp1 = document.getElementById("pop-rules");
-
+const howToPlay  = document.getElementById('how-play')
+const popUp1     = document.getElementById("pop-rules");
 const closePopUp = document.getElementById('demo1')
-
-const winPopup = document.getElementById('win-popup')
-const lostPopup = document.getElementById('lost-popup')
-const drawPopup = document.getElementById('draw-popup')
+const winPopup   = document.getElementById('win-popup')
+const lostPopup  = document.getElementById('lost-popup')
+const drawPopup  = document.getElementById('draw-popup')
 
 popUp1.style.display = 'none'
 
@@ -57,35 +53,42 @@ function rollDices(){
   let p1CurrentScore = "";
   let p2CurrentScore = "";
 
-    one = Math.round(Math.random()*5) + 1;
-    two = Math.round(Math.random()*5) + 1;
+    one   = Math.round(Math.random()*5) + 1;
+    two   = Math.round(Math.random()*5) + 1;
     three = Math.round(Math.random()*5) + 1;
-    four = Math.round(Math.random()*5) + 1;
+    four  = Math.round(Math.random()*5) + 1;
 
-    dice1.src = "images/dice-" + one   + ".png";
-    dice2.src = "images/dice-" + two   + ".png";
-    dice3.src = "images/dice-" + three + ".png";
-    dice4.src = "images/dice-" + four  + ".png";
+    dice1.src = "code/images/dice-" + one   + ".png";
+    dice2.src = "code/images/dice-" + two   + ".png";
+    dice3.src = "code/images/dice-" + three + ".png";
+    dice4.src = "code/images/dice-" + four  + ".png";
 
-    if(one === 1){
+    if(one === 1)
+    {
         p1CurrentScore = 0;
-    }else if(two === 1){
+    }else if(two === 1)
+    {
         p1CurrentScore = 0;
     }else{
         p1CurrentScore = one + two;
     }
-    if(one === two){
+    if(one === two)
+    {
         p1CurrentScore = (one + two) * 2;
     }
-    if(three === 1){
+    if(three === 1)
+    {
         p2CurrentScore = 0;
         
-    }else if(four === 1){
+    }else if(four === 1)
+    {
         p2CurrentScore = 0;
-    }else{
+    }else
+    {
         p2CurrentScore = three + four;
     }
-    if(three === four){
+    if(three === four)
+    {
         p2CurrentScore = (three + four) * 2;
     }
 
